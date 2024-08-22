a6237296d68ba2891d3b3595e7077c65ad8cd7d0
Ilhomjon <futurewithme1221@gmail.com>
2024-08-22 17:24:28 +0500

    design: Created delivery address layout

--- components/DeliveryAddressHeader.tsx
+++ components/DeliveryAddressHeader.tsx
@@ -1,13 +1,14 @@
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { Pressable, View } from "react-native";
+import { Pressable, View, Text } from "react-native";
 
 function DeliveryAddressHeader() {
-  
+
     return (
-        <View className='bg-blue-200 h-14 rounded-b-2xl justify-center items-center pt-5 mb-2'>
-                   <Pressable onPress={() => router.dismiss(1)} className="absolute left-3 bottom-2"
-                ><Ionicons name="chevron-back-outline" size={20} color='blue'/></Pressable>
+        <View className='bg-blue-200 h-12 rounded-b-2xl justify-center items-center mb-2'>
+            <Text className='text-black text-xl font-serif '>Delivery</Text>
+            <Pressable onPress={() => router.dismiss(1)} className="absolute left-3 top-4"
+            ><Ionicons name="chevron-back-outline" size={20} color='blue' /></Pressable>
         </View>
     )
 }
